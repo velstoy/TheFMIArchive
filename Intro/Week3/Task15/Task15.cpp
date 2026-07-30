b6a0ddd07518114ac856236f32ff4fac439f6749
Pavel Stoyanov <114023028+velstoy@users.noreply.github.com>
2026-07-30 20:45:28 +0300

    Completed Week3 Task15

---
+++ Intro/Week3/Task15/Task15.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (j > i) 
+			{
+				std::cout << 0 << " ";
+			}
+			else
+			{
+				std::cout << 1 << " ";
+			}
+		}
+		std::cout << std::endl;
+	}
+}
