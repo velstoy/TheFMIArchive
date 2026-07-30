ffb57081b1476919a022f6100721325115fd4cd9
Pavel Stoyanov <114023028+velstoy@users.noreply.github.com>
2026-07-30 20:48:44 +0300

    Completed Week3 Task16

---
+++ Intro/Week3/Task16/Task16.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			std::cout << j + 1 << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	for (int i = n - 1; i >= 1; i--)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			std::cout << j + 1 << " ";
+		}
+
+		std::cout << std::endl;
+	}
+}
