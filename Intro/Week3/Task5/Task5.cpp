5530f18c1e8b8675b09ca9150924cc1c70045815
Pavel Stoyanov <114023028+velstoy@users.noreply.github.com>
2026-07-30 20:07:36 +0300

    Completed Week3 Task5

--- Intro/Week3/Task5/Task5.cpp
+++ Intro/Week3/Task5/Task5.cpp
@@ -1,20 +1,22 @@
-// Task5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int num;
+	std::cin >> num;
+
+	if (num < 0)
+	{
+		std::cout << "Number cannot be negative!" << std::endl;
+		return -1;
+	}
+
+	int res = 1;
+
+	for (size_t i = 1; i <= num; i++)
+	{
+		res *= i;
+	}
+
+	std::cout << res << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
