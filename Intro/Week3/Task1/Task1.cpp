b1632be2cca2f4f69d5d651c2883ff506aa90bce
Pavel Stoyanov <114023028+velstoy@users.noreply.github.com>
2026-07-30 19:43:17 +0300

    Completed Week3 Task1

---
+++ Intro/Week3/Task1/Task1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main()
+{
+	int n;
+	std::cin >> n;
+
+	switch (n)
+	{
+	case 1:
+		std::cout << "Monday" << std::endl;
+		break;
+	case 2:
+		std::cout << "Tuesday" << std::endl;
+		break;
+	case 3:
+		std::cout << "Wednesday" << std::endl;
+		break;
+	case 4:
+		std::cout << "Thursday" << std::endl;
+		break;
+	case 5:
+		std::cout << "Friday" << std::endl;
+		break;
+	case 6:
+		std::cout << "Saturday" << std::endl;
+		break;
+	case 7:
+		std::cout << "Sunday" << std::endl;
+		break;
+	default:
+		std::cout << "Invalid day!" << std::endl;
+		break;
+	}
+}
